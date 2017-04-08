65c73b3dd40838511f428dc1e24c26cd70cbe8e6
13030367 Ariel Varodz <13030367@itcelaya.edu.mx>
2017-04-07 22:22:31 -0500

    Practica 11, formulario con validator

---
+++ Practicas/Practic11/index.php
@@ -0,0 +1,22 @@
+<?php ?>
+
+<form action="index.php" method="POST"/>
+
+      <input type="text" id="hola"  name="nombre"/>
+      <input type="password" id="pwd"  name="password"/>
+
+
+
+</form>
+
+<pre>
+
+<?php
+
+print_r($_POST);
+
+
+
+
+ ?>
+</pre>
